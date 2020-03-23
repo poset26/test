876fa71b7d1890208e55fb2e0e41b20d9dabe677
poset26 <60416607+poset26@users.noreply.github.com>
2020-03-23 15:36:28 -0300

    Add files via upload

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main(void)
+{
+    FILE *login = fopen("login.csv", "a");
+    char *user = malloc(20);
+    char *paswd = malloc(20);
+    printf("Username: ");
+    scanf("%s",user);
+    printf("Password: ");
+    scanf("%s", paswd);
+    fprintf(login,"%s, %s\n",user, paswd);
+}
